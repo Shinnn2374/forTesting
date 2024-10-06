f069c0bb79ef16ea38f14c8b4e457f6f3a9bd187
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2024-10-06 19:46:26 +0300

    ...

---
+++ check/src/test/java/AppleTest.java
@@ -0,0 +1,5 @@
+public class AppleTest extends BaseTest
+{
+    private static final String BASE_URL = "https://appleinsider.ru/";
+    private static final String SEARCH_WORD = "iphone-13";
+}
---
+++ check/src/test/java/MainPage.java
@@ -0,0 +1,22 @@
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.Keys;
+
+import static com.codeborne.selenide.Selenide.$x;
+
+public class MainPage
+{
+    private final SelenideElement clickOnButon = $x("//input[@type=\"text\"]");
+
+    public MainPage(String url)
+    {
+        Selenide.open(url);
+    }
+
+    public SearchPage search(String search)
+    {
+        clickOnButon.setValue(search);
+        clickOnButon.sendKeys(Keys.ENTER);
+    }
+
+}
