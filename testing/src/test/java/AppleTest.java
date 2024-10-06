231839c68713c87adc70cfff6892e946588efca6
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2024-10-06 15:10:47 +0300

    ...

---
+++ testing/src/test/java/AppleTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+public class AppleTest extends BaseTest
+{
+    private static final String BASE_URL = "https://appleinsider.ru/";
+    private static final String SEARCH_STRING = "Чем iPhone 13 отличается от iPhone 12";
+
+    @Test
+    public void checkHref()
+    {
+
+    }
+}
---
+++ testing/src/test/java/MainPage.java
@@ -0,0 +1,23 @@
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$x;
+
+/**
+ * Главная страница сайта appleinsider.ru
+ */
+
+public class MainPage
+{
+    private final SelenideElement searchButton = $x("//form");
+
+    public void clickOnSearch()
+    {
+        searchButton.click();
+    }
+
+    public void openWebSite(String url)
+    {
+        Selenide.open(url);
+    }
+}
