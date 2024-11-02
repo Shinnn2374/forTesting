8d7291a86653f044710e1877175e0c4afe8f3200
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2024-11-02 17:27:58 +0300

    ...

--- gradleTest/build.gradle.kts
+++ gradleTest/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.3.5"
 	id("io.spring.dependency-management") version "1.1.6"
+	id("io.qameta.allure") version "2.9.6"
+
 }
 
 group = "com.example"
@@ -30,6 +32,7 @@ dependencies {
 	implementation("com.google.code.gson:gson:2.11.0")
 
 
+	testImplementation("org.aspectj:aspectjweaver:1.9.5")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	testCompileOnly("org.projectlombok:lombok")
@@ -38,8 +41,9 @@ dependencies {
 	testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.2")
 	testImplementation("junit:junit:4.13.2")
 
+	// allure
 	testImplementation("io.qameta.allure:allure-junit5:2.12.1")
-	testImplementation("io.qameta.allure:allure-commandLine:2.12.1")
+	testImplementation("io.qameta.allure:allure-commandline:2.12.1")
 	testImplementation("io.qameta.allure:allure-assertj:2.12.1")
 	testImplementation("io.qameta.allure:allure-rest-assured:2.12.1")
 	testImplementation("io.qameta.allure:allure-java-commons:2.12.1")
