7f702ae076e33286972f1c596deb290de4107881
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2024-11-04 00:20:12 +0300

    ...

--- gradleTest/src/test/java/com/example/gradleTest/cucumber/steps/BeforeStep.java
+++ gradleTest/src/test/java/com/example/gradleTest/cucumber/steps/BeforeStep.java
@@ -1,5 +1,6 @@
 package com.example.gradleTest.cucumber.steps;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import io.cucumber.java.en.Given;
 
@@ -7,6 +8,7 @@ public class BeforeStep {
     @Given("Открываем сайт {string}")
     public void openWebSite(String url)
     {
+        Configuration.timeout = 60_000;
         Selenide.open(url);
     }
 }
--- gradleTest/src/test/java/com/example/gradleTest/cucumber/steps/KlavaGonkiStep.java
+++ gradleTest/src/test/java/com/example/gradleTest/cucumber/steps/KlavaGonkiStep.java
@@ -1,6 +1,8 @@
 package com.example.gradleTest.cucumber.steps;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.When;
 
 import static com.codeborne.selenide.Selenide.$;
@@ -10,12 +12,10 @@ public class KlavaGonkiStep {
 
     private final SelenideElement closeButton = $x("//input[@value='Закрыть']");
     private final SelenideElement startButton = $x("//a[@id='host_start']");
+    private final SelenideElement highLight = $x("//span[@id='typefocus']");
+    private final SelenideElement inputField = $x("//input[@id='inputtext']");
+    private final SelenideElement afterFocus = $x("//span[@id='afterfocus']");
 
-
-    static
-    {
-        
-    }
 
     @When("Начинаем игру")
     public void startGame()
@@ -24,6 +24,22 @@ public class KlavaGonkiStep {
         if (startButton.isDisplayed())
         {
             startButton.click();
+        }
+    }
+
+    @And("Ждем начала игры")
+    public void waitForStartGame()
+    {
+        highLight.click();
+    }
+
+    @And("Вводим подсвеченное слово в цикле")
+    public void insertFocusWord()
+    {
+        while(true)
+        {
+            String currentWord = highLight.getText();
+            String
         }
     }
 }
